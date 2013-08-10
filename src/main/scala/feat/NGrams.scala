4ebeb7fe3cf6728676cd465af6f934fcd8f35f9e
Evan Sparks <sparks@cs.berkeley.edu>
2013-08-09 19:44:35 -0700

    Adding stopwords, removing multigram support, and fixing empty token bug.

--- src/main/scala/feat/NGrams.scala
+++ src/main/scala/feat/NGrams.scala
@@ -12,10 +12,10 @@ object NGrams extends FeatureExtractor with Serializable {
   /**
    * Helper method used to build total frequency table of all n-grams in the corpus.
    */
-  def buildDictionary(table: MLTable, numGrams: Int, numFeatures: Int): Seq[(String,Int)] = {
+  def buildDictionary(table: MLTable, numGrams: Int, numFeatures: Int, stopWords: Set[String]): Seq[(String,Int)] = {
 
     //This is the classic Word Count, sorted descending by frequency.
-    val freqTable = table.flatMap(rowToNgramRows(_, numGrams))
+    val freqTable = table.flatMap(rowToNgramRows(_, numGrams, stopWords))
                         .map(row => MLRow(row(0),1))
                         .reduceBy(Seq(0), (x,y) => MLRow(x(0),x(1).toNumber+y(1).toNumber))  //Run a reduceByKey on the first column.
                         .sortBy(Seq(1), false)    //Run a sortby on the second column.
@@ -31,27 +31,28 @@ object NGrams extends FeatureExtractor with Serializable {
   def normalize(raw: String) = raw.replaceAll("[^a-zA-Z0-9]+","").toLowerCase
 
   //Does the actual N-Gram extraction from a string.
-  def ngrams(str: String, n: Int, sep: String="_"): Set[String] = {
-    //Lowercase and split on whitespace.
-    val toks = str.toLowerCase.replaceAll(sep,"").split("\\s+").map(normalize)
+  def ngrams(str: String, n: Int, stopWords: Set[String] = Set[String](), sep: String="_"): Set[String] = {
+    //Lowercase and split on whitespace, and filter out empty tokens and stopwords.
+    val toks = str.toLowerCase.replaceAll(sep,"").split("\\s+").map(normalize).filter(_ != "").filter(!stopWords.contains(_))
 
     //Produce the ngrams.
-    val ngrams = (for( i <- 1 to n) yield toks.sliding(i).map(p => p.mkString(sep))).flatMap(x => x)
+    //val ngrams = (for( i <- 1 to n) yield toks.sliding(i).map(p => p.mkString(sep))).flatMap(x => x)
+    val ngrams =  toks.sliding(n).map(p => p.mkString(sep))
 
     ngrams.toSet
   }
 
   //Responsible for tokenizing data for dictionary calculation.
-  def rowToNgramRows(row: MLRow, n: Int): Seq[MLRow] = {
-    val grams = ngrams(row(0).toString, n)
+  def rowToNgramRows(row: MLRow, n: Int, stopWords: Set[String]): Seq[MLRow] = {
+    val grams = ngrams(row(0).toString, n, stopWords)
     grams.map(s => MLRow(MLValue(s))).toSeq
   }
 
   //The main map function - given a row and a dictionary, return a new row which is an n-gram feature vector.
-  def rowToFilteredNgrams(row: MLRow, dict: Seq[(String,Int)], n: Int): MLRow = {
+  def rowToFilteredNgrams(row: MLRow, dict: Seq[(String,Int)], n: Int, stopWords: Set[String]): MLRow = {
 
     //Pull out the ngrams for a specific string.
-    val x = ngrams(row(0).toString, n)
+    val x = ngrams(row(0).toString, n, stopWords)
 
     //Given my dictionary, create an Index,Value pair that indicates whether or not
     //the ngram was present in the string.
@@ -68,13 +69,13 @@ object NGrams extends FeatureExtractor with Serializable {
    * @param k Top-k features to keep.
    * @return Table of featurized data.
    */
-  def extractNGrams(in: MLTable, n: Int=1, k: Int=20000): MLTable = {
+  def extractNGrams(in: MLTable, n: Int=1, k: Int=20000, stopWords: Set[String] = Set[String]()): MLTable = {
 
     //Build dictionary.
-    val dict = buildDictionary(in, n, k)
+    val dict = buildDictionary(in, n, k, stopWords)
 
     //Extract the ngrams and pack into collection of rows.
-    val out = in.map(rowToFilteredNgrams(_, dict, n))
+    val out = in.map(rowToFilteredNgrams(_, dict, n, stopWords))
 
     //Set the column names of the table to match the NGram features.
     out.setColNames(dict.map(_._1))
